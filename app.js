bd47a24a645a196fc686e115b10b6dda91eb064d
Ronicito <ro.rodriguezbo@gmail.com>
2025-05-20 21:46:25 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// 💡 Primero creamos la app
+const app = express();
+
+// 💡 Luego los middlewares
+app.use(cors());
+app.use(express.json());
+
+// 💡 Luego importamos las rutas
+const distributorRoutes = require('./lib/modules/distributor/distributor.routes');
+const vehicleRoutes = require('./lib/modules/distributor/vehicle.routes');
+const assignmentRoutes = require('./lib/modules/assignment/assignment.routes');
+const pedidoRoutes = require('./lib/modules/pedido/pedido.routes'); 
+const mapaRoutes = require('./lib/modules/maps/maps.routes');
+
+// 💡 Luego usamos las rutas
+app.use('/api/distribuidores', distributorRoutes);
+app.use('/api/vehiculos', vehicleRoutes);
+app.use('/api/asignacion', assignmentRoutes);
+app.use('/api/pedidos', pedidoRoutes); 
+require('dotenv').config();
+app.use('/api/mapa', mapaRoutes);
+
+// Ruta base de prueba
+app.get('/', (req, res) => {
+  res.send('Backend de Distribución de Zapatos en marcha 🥿🚚');
+});
+
+// Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
